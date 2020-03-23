b37c37214d9929f5b9c77a7e2fd1dde2555a65e3
Lisa Jung <lisahjung@gmail.com>
2020-03-23 22:36:31 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -6,6 +6,36 @@ PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
 KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+=begin
 def assembled_aoh
   aoh=[
     DON_G,
@@ -33,3 +63,4 @@ def aoh_update(aoh, row, key, new_value)
   aoh[row][key]=new_value
   aoh
 end
+=end
