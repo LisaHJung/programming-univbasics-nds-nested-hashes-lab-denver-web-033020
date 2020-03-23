73029559ccf7fd255724e022249d402ba43ee51c
Lisa Jung <lisahjung@gmail.com>
2020-03-23 22:44:32 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -62,32 +62,5 @@ end
 
 
 
-=begin
-def assembled_aoh
-  aoh=[
-    DON_G,
-    JOELLE_VD,
-    PAT_M,
-    KATE_G,
-    BRUCE_G]
-end
 
-def literal_aoh
-  aoh=[
-     { name:  "Don Gately", occupation: "Live-in Staff" },
-     { name:  "Joelle van Dyne", occupation: "Radio Personality" },
-     { name:  "Pat Monteseian", occupation: "Staff" },
-     {name:  "Kate Gompert", occupation: "None" },
-     { name:  "Bruce Green", occupation: "Fan of Mildred" }
-]
-end
-
-def aoh_lookup(aoh, row, key)
-  aoh[row][key]
-end
-
-def aoh_update(aoh, row, key, new_value)
-  aoh[row][key]=new_value
-  aoh
-end
 =end
