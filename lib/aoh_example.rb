ce10be69b6d550687c97cc00a0b41ba17b172823
Lisa Jung <lisahjung@gmail.com>
2020-03-23 22:38:19 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -6,7 +6,9 @@ PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
 KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
-
+def assembled aoh
+  
+  
 
 
 
